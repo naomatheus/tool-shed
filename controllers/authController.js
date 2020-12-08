f35b476bfd8917b1a38b1fe8fc429fc01f41f0b5
claytonschoonoversmith@gmail.com <claytonschoonoversmith@gmail.com>
2020-12-07 22:07:28 -0600

    fixing code error in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const express 			= requre('express');
+const express 			= require('express');
 const router 			= express.Router();
 const mongoose 			= require('mongoose');
 const methodOverride 	= require('method-override');
